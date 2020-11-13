a34ace6dd98796dc3149966891031b3fe4bb8d9c
BrandonVA <codebva@gmail.com>
2020-11-12 23:52:05 -0800

    Added routes html routes to the server.

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
+
+// enable app to use data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require('./routes/htmlRoutes')(app)
 
 app.listen(PORT, ()=> {
     console.log(`Listing at port: ${PORT}`);
