71086761b336a88d9daa9c8185a7c893ccd6d8f4
BrandonVA <codebva@gmail.com>
2020-11-13 14:57:40 -0800

    Set up a static path to serve out the files in the public folder.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 
 
 const app = express();
@@ -9,9 +10,15 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// setting up static paths
+app.use(express.static(path.join(__dirname, 'public')));
 
-require('./routes/htmlRoutes')(app)
+
+
+
 require('./routes/apiRoutes')(app)
+require('./routes/htmlRoutes')(app)
+
 
 app.listen(PORT, ()=> {
     console.log(`Listing at port: ${PORT}`);
