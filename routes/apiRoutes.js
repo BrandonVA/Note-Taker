829e355893ef41dc7b239b8ea6d144bfffd36d14
BrandonVA <codebva@gmail.com>
2020-11-13 14:59:05 -0800

    Set up get call for api/notes to send out the db.json file parsed.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 
 module.exports = app => {
+    
 
-    // Testing out how to readFiles and convert to usable data
-    fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
-        if (err) throw err;
-        let notesData = JSON.parse(data);
-        console.log(notesData);
+    app.get('/api/notes', (req, res) => {
+        fs.readFile(path.join(__dirname, '../db/db.json'),  (err, data) => {
+            if (err) throw err;
+            res.json(JSON.parse(data))
+        })
     })
+
+
 }
