49b499d4c921a7a810a78abb4fc96d4397a99319
BrandonVA <codebva@gmail.com>
2020-11-13 16:39:46 -0800

    Added a post method to add more notes to the db. also rem

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
+const addNote = require('./addNote')
+
+// Reading file and converting to JSON
+const dbRawData = fs.readFileSync(path.join(__dirname, '../db/db.json'));
+const dbData = JSON.parse(dbRawData);
+
+
 module.exports = app => {
+
     
 
     app.get('/api/notes', (req, res) => {
-        fs.readFile(path.join(__dirname, '../db/db.json'),  (err, data) => {
-            if (err) throw err;
-            res.json(JSON.parse(data))
-        })
+        res.json(dbData)
     })
+
+    app.post('/api/notes', (req, res) => {
+        const newNote = req.body;
+        addNote(newNote, dbData)
+        res.send(dbData)
+    })
+
+    // app.post('/api/notes', (req, res) => {
+
+    // })
 
 
 }
