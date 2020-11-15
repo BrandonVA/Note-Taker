8434ecaaadaf097a42427d1c2a7a4d2ba3e783db
BrandonVA <codebva@gmail.com>
2020-11-14 16:00:55 -0800

    Cleaned up code.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-const addNote = require('./addNote');
-const removeNote = require('./removeNote')
+const addNote = require('./scripts/addNote');
+const removeNote = require('./scripts/removeNote')
 
 // Reading db file and converting to JSON
 const dbRawData = fs.readFileSync(path.join(__dirname, '../db/db.json'));
 let dbData = JSON.parse(dbRawData);
 
 
-console.log(dbData.length);
-
 let idCounter = 1;
 
 if (dbData.length > 0) { 
      idCounter =  dbData[dbData.length -1 ].id +1
 } ;
-
-let cache = '[{"title":"Test Title","text":"Test text","id":1},{"title":"Test Title2","text":"Test text2","id":2},{"title":"note 3","text":"hello ","id":3},{"title":"note 4","text":"this is the number 4 ","id":4}]'
 
 module.exports = app => {
 
@@ -39,16 +35,10 @@ module.exports = app => {
     app.delete('/api/notes/:id', (req, res) => {
 
         const noteToDelete = req.params.id
-
         let new_db = removeNote(noteToDelete, dbData);
         dbData = new_db
         res.send(dbData);
 
-        // console.log(`This is the note ID: ${noteToDelete}`);
-
-        // const updatedNotes = dbData.filter(item => parseInt(item.id) !== parseInt(noteToDelete))
-
-        // console.log(updatedNotes);
     })
 
 
